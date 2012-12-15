fbf7bc294319ab2b75c4e8f25b652bc698a7f870
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2012-12-15 15:07:54 -0500

    Started to move button debouncing into general library.
    Added basic motor positioning functions.

--- psubot.c
+++ psubot.c
@@ -1,19 +1,9 @@
 
 #include "psubot.h"
 
-void psubot_init( void ) {
+int gi_debounce_counter_button = 0;
 
-   /* Setup Motor I/O */
-   P1DIR = 0;
-   P1DIR |= EYE_R;
-   P1DIR |= EYE_L;
-   P1DIR |= EYE_ON;
-   
-   /* Setup LED I/O */
-   P2DIR = 0;
-   P2DIR |= LED_RED;
-   P2DIR |= LED_GREEN;
-   P2DIR |= LED_BLUE;
+void psubot_init( void ) {
 
    /* Turn off all indicators.*/
    P1OUT = 0;
@@ -29,6 +19,75 @@ void psubot_button_enable( void ) {
 
    /* Listen for button release. */
    P2IES &= ~BUTTON;
+}
+
+void psubot_eye_enable( void ) {
+
+   /* Setup Motor I/O */
+   P1DIR = 0;
+   P1DIR |= EYE_R;
+   P1DIR |= EYE_L;
+   P1DIR |= EYE_ON;
+   
+   /* Setup LED I/O */
+   P2DIR = 0;
+   P2DIR |= LED_RED;
+   P2DIR |= LED_GREEN;
+   P2DIR |= LED_BLUE;
+}
+
+/* Parameters:                                                                *
+ *    i_pos_in - The percentage to the left to position the eye.              */
+void psubot_eye_pos( int i_pos_in ) {
+   //int i_pos_target = 0;
+   int i;
+
+   /* Turn on the motor. */
+   P1OUT |= EYE_ON;
+
+   /* Move the eye to position zero. */
+   P1OUT |= EYE_R;
+   while( !(P2IN & EYE_SENSE) ) {
+      SLEEP( 10 );
+   }
+   P1OUT &= ~EYE_R;
+
+   /* Turn off the motor. */
+   P1OUT &= ~EYE_ON;
+
+   psubot_eye_left( i_pos_in );
+}
+
+/* Purpose: Push the eye to the robot's left by the given increment.          */
+void psubot_eye_left( int i_pos_in ) {
+   int i;
+
+   /* Turn on the motor. */
+   P1OUT |= EYE_ON;
+
+   /* Move the eye to the selected position. */
+   P1OUT |= EYE_L;
+   for( i = 0 ; i < i_pos_in ; i++ ) {}
+   P1OUT &= ~EYE_L;
+
+   /* Turn off the motor. */
+   P1OUT &= ~EYE_ON;
+}
+
+/* Purpose: Push the eye to the robot's right by the given increment.         */
+void psubot_eye_right( int i_pos_in ) {
+   int i;
+
+   /* Turn on the motor. */
+   P1OUT |= EYE_ON;
+
+   /* Move the eye to the selected position. */
+   P1OUT |= EYE_R;
+   for( i = 0 ; i < i_pos_in ; i++ ) {}
+   P1OUT &= ~EYE_R;
+
+   /* Turn off the motor. */
+   P1OUT &= ~EYE_ON;
 }
 
 /* Purpose: Wait for stimuli.                                                 */
--- psubot.h
+++ psubot.h
@@ -9,6 +9,7 @@
 #define LED_BLUE  BIT2
 
 #define BUTTON    BIT3
+#define EYE_SENSE BIT4
 
 /* Eye directions are from the robot's point of view. */
 
@@ -16,13 +17,21 @@
 #define EYE_L     BIT3
 #define EYE_ON    BIT5
 
-/* 5 = 1 second */
-#define DEBOUNCE_TIMEOUT   2
+#define EYE_MAX_CYCLES_L 27000
+
+/* = Macros = */
+
+#define BUTTON_ENABLE() extern int gi_debounce_counter_button;
+#define SLEEP( cycles_in ) __delay_cycles( cycles_in );
 
 /* = Function Prototypes = */
 
 void psubot_init( void );
 void psubot_button_enable( void );
+void psubot_eye_enable( void );
+void psubot_eye_pos( int );
+void psubot_eye_left( int );
+void psubot_eye_right( int );
 void psubot_wait( void );
 
 #endif /* PSUBOT_H */
