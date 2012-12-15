cec47d5a39b6604800f44d142bc76d97165a60c4
indigoparadox <indigo.repo@interfinitdynamics.info>
2012-12-15 15:07:54 -0500

    Started to move button debouncing into general library.
    Added basic motor positioning functions.

--- test.c
+++ test.c
@@ -1,8 +1,12 @@
 
 #include "psubot.h"
 
-int i_led_current = LED_RED,
-   i_debounce_counter = 0;
+/* 5 = 1 second */
+#define DEBOUNCE_TIMEOUT   5
+
+BUTTON_ENABLE();
+
+int i_led_current = LED_RED;
 
 int main( void ) {
    int i;
@@ -13,19 +17,11 @@ int main( void ) {
 
    psubot_button_enable();
 
-   P2OUT = i_led_current;
+   psubot_eye_enable();
+   psubot_eye_pos( EYE_MAX_CYCLES_L );
+   psubot_eye_pos( EYE_MAX_CYCLES_L / 2 );
 
-   /* Move the eye around. */
-   P1OUT |= EYE_ON;
-   P1OUT |= EYE_R;
-   for( i = 0 ; i < 20000 ; i++ ) {
-   }
-   P1OUT &= ~EYE_R;
-   P1OUT |= EYE_L;
-   for( i = 0 ; i < 20000 ; i++ ) {
-   }
-   P1OUT &= ~EYE_L;
-   P1OUT &= ~EYE_ON;
+   P2OUT = i_led_current;
 
    psubot_wait();
 
@@ -34,54 +30,56 @@ int main( void ) {
 
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2( void ) {
-   P2IFG &= ~BUTTON;
-   P2IE &= ~BUTTON;
+   if( P2IFG & BUTTON ) {
+      /* Reset button status. */
+      P2IFG &= ~BUTTON;
+      P2IE &= ~BUTTON;
 
-   if( i_debounce_counter ) {
-      return;
+      if( gi_debounce_counter_button ) {
+         return;
+      }
+
+      /* Start the debounce timer. */
+      gi_debounce_counter_button = DEBOUNCE_TIMEOUT;
+      BCSCTL3 |= LFXT1S_2;
+      BCSCTL1 |= DIVA_1;
+      TA0CCR0 = 1200;
+      TA0CCTL0 = 0x10;
+      TA0CTL = TASSEL_1 + MC_1;
+      TACCTL0 |= CCIE;
+
+      psubot_eye_pos( EYE_MAX_CYCLES_L / 2 );
+
+      switch( i_led_current ) {
+         case LED_RED:
+            i_led_current = LED_GREEN;
+            break;
+
+         case LED_GREEN:
+            i_led_current = LED_BLUE;
+            break;
+   
+         case LED_BLUE:
+            i_led_current = LED_RED;
+            break;
+
+      }
+
+      P2OUT = i_led_current;
    }
-
-   /* Start the debounce timer. */
-   i_debounce_counter = DEBOUNCE_TIMEOUT;
-   BCSCTL3 |= LFXT1S_2;
-   BCSCTL1 |= DIVA_1;
-   TA0CCR0 = 1200;
-   TA0CCTL0 = 0x10;
-   TA0CTL = TASSEL_1 + MC_1;
-   TACCTL0 |= CCIE;
-
-   switch( i_led_current ) {
-      case LED_RED:
-         i_led_current = LED_GREEN;
-         break;
-
-      case LED_GREEN:
-         i_led_current = LED_BLUE;
-         break;
- 
-      case LED_BLUE:
-         i_led_current = LED_RED;
-         break;
-
-   }
-
-   P2OUT = i_led_current;
-
-   /* wake up the main program */
-   //__bic_SR_register_on_exit( LPM0_bits );
 }
 
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A0( void ) {
    TACCTL0 &= ~CCIFG;
 
-   if( i_debounce_counter ) {
+   if( gi_debounce_counter_button ) {
       /* Don't accept button presses yet. */
       P2IFG &= ~BUTTON;
-      i_debounce_counter--;
+      gi_debounce_counter_button--;
    } else {
       /* Debounce complete. */
-      i_debounce_counter = 0;
+      gi_debounce_counter_button = 0;
       TACCTL0 &= ~ CCIE;
       P2IE |= BUTTON;
    }
