2b9616cb4dce1b1226d42d9f14d050bd4dec2030
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2012-12-27 17:31:32 -0500

    Converted gamepad client to use native Bluetooth libraries.

--- psubot_gamepad.py
+++ psubot_gamepad.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-import pygame
-import serial, time
+import pygame, bluetooth
+
+psubot_address = '00:18:a1:12:0f:f9'
 
 pygame.init()
 
@@ -12,22 +13,26 @@ gamepad = None
 for i in range( 0, pygame.joystick.get_count() ):
    j = pygame.joystick.Joystick( i )
    if 'USB Gamepad' == j.get_name().strip():
-      print "Found " + j.get_name()
+      print "Found " + j.get_name() + "."
       gamepad = j
       gamepad.init()
 
       gamepad_buttons = {
          0: {
             'down': lambda: psubot_command( 'LED RED' ),
+            'up': lambda: do_nothing(),
          },
          1: { 
             'down': lambda: psubot_command( 'LED GREEN' ),
+            'up': lambda: do_nothing(),
          },
          2: {
             'down': lambda: psubot_command( 'LED BLUE' ),
+            'up': lambda: do_nothing(),
          },
          3: {
             'down': lambda: psubot_command( 'BEEP 100 100' ),
+            'up': lambda: do_nothing(),
          },
          4: {
             'down': lambda: psubot_command( 'EYE L' ),
@@ -48,6 +53,15 @@ for i in range( 0, pygame.joystick.get_count() ):
       }
 
       gamepad_axes = {
+         0: {
+            -1.0: lambda: do_nothing(),
+         },
+         1: {
+            0.0: lambda: do_nothing(),
+         },
+         2: {
+            0.0: lambda: do_nothing(),
+         },
          3: { 
             -1.0: lambda: psubot_command( 'DRIVE L' ),
             1.0: lambda: psubot_command( 'DRIVE R' ),
@@ -58,6 +72,9 @@ for i in range( 0, pygame.joystick.get_count() ):
             1.0: lambda: psubot_command( 'DRIVE B' ),
             0.0: lambda: psubot_command( 'DRIVE S' ),
          },
+         5: {
+            -1.0: lambda: do_nothing(),
+         },
       }
 
 
@@ -65,32 +82,29 @@ if None == gamepad:
    print "Joystick not found!"
    die()
 
+try:
+   btsocket = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+   btsocket.connect( (psubot_address, 1) ) 
+except bluetooth.btcommon.BluetoothError:
+   print "Unable to establish connection!"
+   exit( 1 )
+
+print "Connection established. Please press the reset button on the PSUBot."
+
+# TODO: Wait for READY prompt.
+
 print "Ready!"
 
-# Try to connect to the psubot.
-serial_psubot = serial.Serial( '/dev/rfcomm0', 9600, timeout=1 );
-
 def psubot_command( command_in ):
-   global serial_psubot
-   try:
-      serial_psubot.write( command_in + "\r" )
-   except serial.SerialException:
-      while None == serial_psubot:
-         print 'Connection lost. Attempting to reconnect...'
-         serial_psubot = serial.Serial( '/dev/rfcomm0', 9600, timeout=1 );
-      serial_psubot.write( command_in + "\r" )
-   #print command_in
+   global btsocket
+   btsocket.send( command_in + "\r" )
+
+def do_nothing():
+   pass
 
 try:
    while True:
       event = pygame.event.poll()
-      '''for i in range( 0, gamepad.get_numaxes() ):
-         if gamepad.get_axis( i ) != 0.00:
-            #print i
-            pass'''
-      '''for i in range( 0, gamepad.get_numbuttons() ):
-         if gamepad.get_button( i ):
-            print i'''
 
       if event.type == pygame.JOYBUTTONDOWN:
          action = None
