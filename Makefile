2902c1ed79029036b26ba390e620a3662e567273
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2014-06-10 10:35:33 -0400

    Restructured project filenames to be more meaningful.

--- Makefile
+++ Makefile
@@ -3,29 +3,12 @@ CC = msp430-gcc
 
 CFLAGS = -mmcu=msp430g2553 -Wall
 
-BASE_SYSTEM = psubot.o beep.o
+BASE_SYSTEM = botlib.o beep.o
 INTERACTIVE_SYSTEM = uart.o shell.o
 
-all: test_bluetooth
+all: psubot
 
-# test: A general test/debug suite.
-test: test.o $(BASE_SYSTEM) $(INTERACTIVE_SYSTEM)
-	$(CC) $(CFLAGS) -o $@.out $^
-
-# test_bluetooth: A general test/debug suite designed to operate over the
-# 						bluetooth remote serial port.
-test_bluetooth: test_bluetooth.o $(BASE_SYSTEM) $(INTERACTIVE_SYSTEM)
-	$(CC) $(CFLAGS) -o $@.out $^
-
-# test_eyesense: A simple test that will change the eye color to blue when the
-# 					  motor right position sensor is depressed.
-test_eyesense: test_eyesense.o $(BASE_SYSTEM)
-	$(CC) $(CFLAGS) -o $@.out $^
-
-test_eyemotion: test_eyemotion.o $(BASE_SYSTEM)
-	$(CC) $(CFLAGS) -o $@.out $^
-
-test_motors: test_motors.o
+psubot: psubot.o $(BASE_SYSTEM) $(INTERACTIVE_SYSTEM)
 	$(CC) $(CFLAGS) -o $@.out $^
 
 # = Generic Utility Definitions =
@@ -33,11 +16,8 @@ test_motors: test_motors.o
 %.o: %.c
 	$(CC) $(CFLAGS) -o $@ -c $<
 
-#flash: $(FLASH)
-#	mspdebug rf2500 "prog ./$(FLASH).out"
-
 flash:
-	mspdebug rf2500 "prog ./test_bluetooth.out"
+	mspdebug rf2500 "prog ./psubot.out"
 
 clean:
 	rm *.o
--- psubot.c
+++ psubot.c
@@ -1,174 +1,128 @@
 
-#include "psubot.h"
+#include <msp430.h>
 
-#include <stdint.h>
+#include "botlib.h"
+#include "uart.h"
+#include "shell.h"
+#include "beep.h"
 
-//int gi_debounce_counter_button = 0;
-BOOL gb_sleeping = 0;
-int gi_eye_move_loops = 0;
-DRIVING_DIR ge_driving_dir = DRIVING_STOPPED;
+#include <stdlib.h>
 
-void psubot_init( void ) {
+SHELL_ENABLE();
 
-   /* Turn off all indicators.*/
-   P1OUT = 0;
-   P2OUT = 0;
+int i_led_current = LED_RED;
+
+int main( void ) {
+
+   psubot_init();
+
+   psubot_eye_enable();
+   psubot_eye_pos( 50 );
+
+   psubot_wheels_enable();
+
+   P2OUT = i_led_current;
+
+   uart_serial_init();
+
+   beep_init();
+
+   // TODO: Check to see if we're connected.
+   uart_echo( "\r\n+STWMOD=0\r\n" );
+   uart_echo( "\r\n+STNA=PSUBot\r\n" );
+   uart_echo( "\r\n+STAAUTO=1\r\n" );
+   uart_echo( "\r\n+STOAUT=1\r\n" );
+   uart_echo( "\r\n+STPIN=0000\r\n" );
+
+   __delay_cycles( 1000000 );
+   uart_echo( "\r\n+INQ=1\r\n" );
+   __delay_cycles( 1000000 );
+
+   /* uart_echo( "\r\n+CONN=00:10:C6:C3:E6:4D\r\n" ); */
+
+   shell_init();
+
+   /* Play a little tune to signal we're ready. */
+   BEEP_NOTE gae_startup[4] = {
+      BEEP_C, BEEP_E, BEEP_D, BEEP_NULL
+   };
+   beep_string( &gae_startup, 100 );
+
+   /* Go to sleep. */
+   /* TODO: Find a way to sleep without disabling the maintenance timer. */
+   __bis_SR_register( LPM0_bits + GIE );
+
+   return 0;
+}
+
+void command_led( void ) {
+   
+   if( shell_strcmp( "RED", gac_args[1] ) ) {
+      i_led_current = LED_RED;
+   } else if( shell_strcmp( "GREEN", gac_args[1] ) ) {
+      i_led_current = LED_GREEN;
+   } else if( shell_strcmp( "BLUE", gac_args[1] ) ) {
+      i_led_current = LED_BLUE;
+   }
 
    #if 0
-   /* This should be used on button inputs to debounce or something,          *
-    * probably. Our original intention was to use it to control wheel drivine *
-    * before we switched to the new continuous driving model. The timer       *
-    * itself conflicts with the low power modes.                              */
+   switch( i_led_current ) {
+      case LED_RED:
+         i_led_current = LED_GREEN;
+         break;
 
-   /* Enable the maintenance timer. */
-   WDTCTL = WDTPW | WDTTMSEL | WDTCNTCL;
-   /* WDTCTL = WDT_ADLY_1000; */
-   IE1 |= WDTIFG;
-   /* BCSCTL2 = DIVS_2; */
-   _EINT();
-   #else
-   WDTCTL = WDTPW | WDTHOLD;
+      case LED_GREEN:
+         i_led_current = LED_BLUE;
+         break;
+
+      case LED_BLUE:
+         i_led_current = LED_RED;
+         break;
+
+   }
    #endif
+
+   P2OUT = i_led_current;
 }
 
-void psubot_eye_enable( void ) {
-
-   /* Setup Motor I/O */
-   P1DIR = 0;
-   P1DIR |= EYE_R;
-   P1DIR |= EYE_L;
-   
-   /* Setup LED I/O */
-   P2DIR = 0;
-   P2DIR |= LED_RED;
-   P2DIR |= LED_GREEN;
-   P2DIR |= LED_BLUE;
-}
-
-void psubot_wheels_enable( void ) {
-   P1DIR |= WHEEL_R_F;
-   P2DIR |= WHEEL_L_F | WHEEL_L_R | WHEEL_R_R;
-}
-
-/* Parameters:                                                                *
- *    i_pos_in - The percentage to the left to position the eye.              */
-void psubot_eye_pos( int i_pos_in ) {
-   uint8_t i_sense_count = 0;
-   long l_target_pos,
-      l_pos_in = i_pos_in,
-      i;
-
-   /* Figure out the position as a percentage of the eye's max range. */
-   l_target_pos = l_pos_in * EYE_MAX_CYCLES_L / 100;
-
-   /* Move the eye to position zero. */
-   P1OUT |= EYE_R;
-   while( 
-      !(P1IN & EYE_SENSE && i_sense_count >= 200) && 
-      EYE_MAX_LOOPS_R > gi_eye_move_loops
-   ) {
-      __delay_cycles( 10 );
-      gi_eye_move_loops++;
-      
-      /* Don't count the sensor as being held down unless it's being held     *
-       * down!                                                                */
-      if( P1IN && EYE_SENSE ) {
-         i_sense_count++;
-      } else {
-         i_sense_count = 0;
-      }
-   }
-   P1OUT &= ~EYE_R;
-
-   /* Make sure we didn't just time out. */
-   if( EYE_MAX_LOOPS_R <= gi_eye_move_loops ) {
-      psubot_halt();
-   } else {
-      gi_eye_move_loops = 0;
-   }
-
-   psubot_eye_move( EYE_LEFT );
-   for( i = 0 ; i < l_target_pos ; i++ ) {}
-   psubot_eye_move( EYE_STOPPED );
-}
-
-/* Purpose: Push the eye to the robot's left by the given increment.          */
-/* TODO: Keep a global variable with the current position of the eye. Set it  *
- *       to 0 during the eye init phase and increment/decrement it whenever   *
- *       the eye is moved. Don't allow the eye to move out of its operational *
- *       range.                                                               */
-void psubot_eye_move( EYE_DIR e_eye_dir_in ) {
-   switch( e_eye_dir_in ) {
-      case EYE_RIGHT:
-         P1OUT |= EYE_R;
-         break;
-      case EYE_LEFT:
-         P1OUT |= EYE_L;
-         break;
-      case EYE_STOPPED:
-         P1OUT &= ~EYE_L & ~EYE_R;
-         break;
+void command_eye( void ) {
+   if( shell_strcmp( "POS", gac_args[1] ) ) {
+      psubot_eye_pos( atoi( gac_args[2] ) );
+   } else if( shell_strcmp( "R", gac_args[1] ) ) {
+      psubot_eye_move( EYE_RIGHT );
+   } else if( shell_strcmp( "L", gac_args[1] ) ) {
+      psubot_eye_move( EYE_LEFT );
+   } else if( shell_strcmp( "S", gac_args[1] ) ) {
+      psubot_eye_move( EYE_STOPPED );
    }
 }
 
-void psubot_wheel_drive( DRIVING_DIR e_direction_in ) {
-   switch( e_direction_in ) {
-      case DRIVING_FORWARD:
-         P1OUT |= WHEEL_R_F;
-         P2OUT |= WHEEL_L_F;
-         break;
-      case DRIVING_REVERSE:
-         P2OUT |= WHEEL_L_R | WHEEL_R_R;
-         break;
-      case DRIVING_RIGHT:
-         P2OUT |= WHEEL_L_F;
-         break;
-      case DRIVING_RIGHT_PIVOT:
-         P2OUT |= WHEEL_L_F | WHEEL_R_R;
-         break;
-      case DRIVING_LEFT:
-         P1OUT |= WHEEL_R_F;
-         break;
-      case DRIVING_LEFT_PIVOT:
-         P1OUT |= WHEEL_R_F;
-         P2OUT |= WHEEL_L_R;
-         break;
-      case DRIVING_STOPPED:
-         P1OUT &= ~WHEEL_R_F;
-         P2OUT &= ~WHEEL_L_F & ~WHEEL_R_R & ~WHEEL_L_R;
-      default:
-         return;
+void command_beep( void ) {
+   beep( atoi( gac_args[1] ), atoi( gac_args[2] ) );
+}
+
+void command_drive( void ) {
+   if( shell_strcmp( "R", gac_args[1] ) ) {
+      psubot_wheel_drive( DRIVING_RIGHT );
+   } else if( shell_strcmp( "L", gac_args[1] ) ) {
+      psubot_wheel_drive( DRIVING_LEFT );
+   } else if( shell_strcmp( "PR", gac_args[1] ) ) {
+      psubot_wheel_drive( DRIVING_RIGHT_PIVOT );
+   } else if( shell_strcmp( "PL", gac_args[1] ) ) {
+      psubot_wheel_drive( DRIVING_LEFT_PIVOT );
+   } else if( shell_strcmp( "F", gac_args[1] ) ) {
+      psubot_wheel_drive( DRIVING_FORWARD );
+   } else if( shell_strcmp( "B", gac_args[1] ) ) {
+      psubot_wheel_drive( DRIVING_REVERSE );
+   } else if( shell_strcmp( "S", gac_args[1] ) ) {
+      psubot_wheel_drive( DRIVING_STOPPED );
    }
 }
 
-/* Purpose: Halt all activity and blink the internal LED.                     */
-void psubot_halt( void ) {
-   
-   /* Disable all interrupts. */
-   P1IE = 0;
-   P2IE = 0;
-   IE2 = 0;
-
-   /* Turn off all outputs. */
-   P1OUT = 0;
-   P2OUT = 0;
-
-   /* Blink indefinitely. */
-   P1DIR |= ILED;
-   for(;;) {
-      P1OUT |= ILED;
-      __delay_cycles( 250000 );
-      P1OUT &= ~ILED;
-      __delay_cycles( 250000 );
-   }
-}
-
-#if 0
-/* See WDT notes above. */
-#pragma vector=WDT_VECTOR
-__interrupt void psubot_wdt_isr( void ) {
-   P1OUT ^= BIT0;
-   IFG1 &= ~WDTIFG;
-}
-#endif
+SHELL_COMMANDS_BLOCK_START( 4 )
+SHELL_COMMANDS_BLOCK_ITEM( "LED", "[EYE COLOR]", command_led ),
+SHELL_COMMANDS_BLOCK_ITEM( "EYE", "[POS] [INCR] OR [R/L] [INCR]", command_eye ),
+SHELL_COMMANDS_BLOCK_ITEM( "BEEP", "[FREQ] [TIME]", command_beep ),
+SHELL_COMMANDS_BLOCK_ITEM( "DRIVE", "[F/B/R/L/RP/LP] [TIME]", command_drive ),
+SHELL_COMMANDS_BLOCK_END()
 
